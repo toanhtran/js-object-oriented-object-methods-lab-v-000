fd320c8d9d8bedde2759b15eb2deeb63d22c9bd3
ToAnh Tran <toanh.t.tran@gmail.com>
2018-11-04 03:29:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function BoardMember(name, homeState, training) {
   this.homeState = homeState;
   this.training = training;
   this.veto = function() {
-    console.log(`Hello everybody is ${this.name}!`);
+    console.log('No, I must disagree');
   }
   this.sayHi = function() {
     console.log(`Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trainined in ${this.training}.`)
