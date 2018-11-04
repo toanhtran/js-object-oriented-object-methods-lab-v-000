a6a029cc5edb802d363ad48f85671a95c4f728c1
ToAnh Tran <toanh.t.tran@gmail.com>
2018-11-04 03:32:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ function BoardMember(name, homeState, training) {
   this.homeState = homeState;
   this.training = training;
   this.veto = function() {
-    console.log('No, I must disagree');
+    console.log('No, I must disagree')
   }
   this.approve = function() {
-    console.log('You can do that!');
+    console.log('You can do that!')
+  }
+  this.doCharity = function(){
+    console.log('I like to help people.')
   }
   this.sayHi = function() {
     console.log(`Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trainined in ${this.training}.`)
