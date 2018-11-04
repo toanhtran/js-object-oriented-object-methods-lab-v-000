3e69a93dfce362a23abd45ccd2f00ed1e633016c
ToAnh Tran <toanh.t.tran@gmail.com>
2018-11-04 03:30:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ function BoardMember(name, homeState, training) {
   this.veto = function() {
     console.log('No, I must disagree');
   }
+  this.approve = function() {
+    console.log('You can do that!')
+  }
   this.sayHi = function() {
     console.log(`Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trainined in ${this.training}.`)
   }
