54136deec04565aa51a30415193d92a1d0f3f6df
ToAnh Tran <toanh.t.tran@gmail.com>
2018-11-04 03:34:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function BoardMember(name, homeState, training) {
     return('No, I must disagree');
   }
   this.approve = function() {
-    console.log('You can do that!');
+    return('You can do that!');
   }
   this.doCharity = function(){
     console.log('I like to help people.');
